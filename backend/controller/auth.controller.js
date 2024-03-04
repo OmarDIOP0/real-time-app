03c6a8c06aa66dc7c8c4c1986dc787ce6924a547
OmarDIOP0 <odiop6170@gmail.com>
2024-03-04 01:34:36 +0000

    generate_token

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,4 +1,6 @@
 import User from "../modals/user.modal.js";
+import bcrypt from 'bcryptjs';
+import generateTokenUserAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req,res)=>{
     try {
@@ -10,26 +12,36 @@ export const signup = async (req,res)=>{
         if(user){
             return res.status(400).json({error:'User already exist'});
         }
-
+        //Hash Password 
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password,salt);
+        //Avatar
         const boyProfilePic =`https://avatar.iran.liara.run/public/boy/?username=${username}`;
         const girlProfilePic =`https://avatar.iran.liara.run/public/girl/?username=${username}`;
 
         const newUser = new User({
             fullName,
             username,
-            password,
+            password:hashPassword,
             gender,
             profilePic:gender === "male" ? boyProfilePic : girlProfilePic
         });
+        
+       if(newUser){
+        generateTokenUserAndSetCookie(newUser._id,res);
         await newUser.save();
-
         res.status(200).json({
             _id:newUser._id,
             fullName:newUser.fullName,
             username:newUser.username,
             gender:newUser.gender,
+            password:newUser.password,
             profilePic:newUser.profilePic
         })
+       }
+       else{
+        return res.status(400).json({error:'Invalid user data'});
+       }
 
     } catch (error) {
         console.log('Error in signup controller',error.message);
