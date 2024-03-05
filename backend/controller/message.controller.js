b61fb1c37cc4926d13e2c0e9521a3c3411ad6b7b
OmarDIOP0 <odiop6170@gmail.com>
2024-03-05 23:23:43 +0000

    message_controller

--- backend/controller/message.controller.js
+++ backend/controller/message.controller.js
@@ -1,4 +1,33 @@
+import Conversation from "../modals/conversation.modal.js";
+import Message from "../modals/message.modal.js";
 
-export const sendMessage = (req,res) =>{
-    console.log("Message send "+req.params.id);
+export const sendMessage = async(req,res) =>{
+    try {
+        const {message} = req.body;
+        const{id:receiverId}=req.params;
+        const senderId = req.user._id;
+        
+        let conversation = await Conversation.findOne({
+            participants:{$all:[senderId,receiverId]},
+        })
+        if(!conversation){
+            conversation = await Conversation.create({
+                participants:[senderId,receiverId]
+            })
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+        if (newMessage) {
+            conversation.messages.push(newMessage._id);
+        }
+
+        res.status(200).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in mesage controller "+error.message);
+        return res.status(500).json({error:"Internal server error"})
+    }
 }
