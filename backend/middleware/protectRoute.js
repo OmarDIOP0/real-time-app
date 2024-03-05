deb350e8d96fce0b94c947a8dfb8e4b5ca65cfd3
OmarDIOP0 <odiop6170@gmail.com>
2024-03-05 20:49:30 +0000

    middleware_protectRoute

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import User from '../modals/user.modal.js';
+
+const protectRoute = async(req,res,next)=>{
+    try {
+        const token = req.cookies.jwt;
+        if(!token){
+            return res.status(401).json({error:"Unauthorized - No token Provided"});
+        }
+        const decoded = jwt.decode(token,process.env.JWT_SECRET);
+        if(!decoded){
+            return res.status(401).json({error:"Unauthorized Invalid token"});
+        }
+        const user = await User.findById(decoded.userId).select('-passwod');
+
+        if(!user){
+            return res.status(401).json({error:"User not found"});
+        }
+        req.user=user;
+        next();
+        
+    } catch (error) {
+        console.log("Error in protected Routes middleware "+error.message);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
+export default protectRoute;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from "./db/connexionMongoDB.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser()); //parser les cookies du requete
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
