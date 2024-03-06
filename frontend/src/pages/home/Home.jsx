4c68fbc4a368e6372685d4187c08fe67b7f6e76a
OmarDIOP0 <odiop6170@gmail.com>
2024-03-06 23:15:14 +0000

    add_message

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
+import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
     <div className='flex sm:h-[450px] md:h-[550px] overflow-hidden rounded-lg bg-gray-400 bg-clip-padding  backdrop-filter
     backdrop-blur-lg bg-opacity-0'>
      <Sidebar />
+     <MessageContainer />
     </div>
   )
 }
