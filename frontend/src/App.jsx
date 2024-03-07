9000eb723222f59850633fefa38510b3dc22597d
OmarDIOP0 <odiop6170@gmail.com>
2024-03-07 20:12:52 +0000

    config_context_to_route_login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
+        <Route path='/login' element={authUser?<Navigate to='/'/> : <Login />}/>
         <Route path='/signup' element={authUser?<Navigate to='/'/>:<Signup/> }/>
       </Routes>
       <Toaster/>
