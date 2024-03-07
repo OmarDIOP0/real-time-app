93cd0a462c0d83182d5f66c0edd5c6efd974ba8a
OmarDIOP0 <odiop6170@gmail.com>
2024-03-07 20:16:57 +0000

    config_context_to_route_home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,9 +12,9 @@ function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/login' element={authUser?<Navigate to='/'/> : <Login />}/>
-        <Route path='/signup' element={authUser?<Navigate to='/'/>:<Signup/> }/>
+        <Route path='/' element={authUser ? <Home /> : <Navigate to='/login'/>}/>
+        <Route path='/login' element={authUser ? <Navigate to='/'/> : <Login />}/>
+        <Route path='/signup' element={authUser ? <Navigate to='/'/> :<Signup/> }/>
       </Routes>
       <Toaster/>
     </div>
