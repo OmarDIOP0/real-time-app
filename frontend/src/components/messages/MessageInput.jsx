2c6fef5995cac419ea2dd719d39015b754d8abd8
OmarDIOP0 <odiop6170@gmail.com>
2024-03-11 16:06:50 +0000

    send_message_

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsSend} from 'react-icons/bs'
+import useSendMessage from '../../hooks/useSendMessage'
 
 const MessageInput = () => {
+  const [message,setMessage]=useState("");
+  const {loading,sendMessage}=useSendMessage();
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    if(!message) return;
+    sendMessage(message);
+    setMessage("");
+  }
   return (
     <div>
-      <form className='px-4 my-3'>
+      <form className='px-4 my-3' onSubmit={handleSubmit}>
         <div className="w-full relative">
             <input type="text" className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600
-            text-white' placeholder='Send a message'/>
+            text-white' placeholder='Send a message' value={message} 
+              onChange={(e)=>setMessage(e.target.value)}/>
             <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3 text-white'>
-                 <BsSend/> 
+                 {loading? <div className='loading loading-spinner'></div>:<BsSend/> }
             </button>
         </div>
       </form>
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -7,8 +7,6 @@ import useConversation from '../../zustand/useConversation';
 
 const Conversations = () => {
   const{loading,conversations}=useGetConversation();
-
-  console.log("conversation",conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
      {conversations.map((conversation,index)=>(
