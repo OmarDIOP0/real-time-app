faff499f1fbf9f847693e188a1b699031f4ec707
OmarDIOP0 <odiop6170@gmail.com>
2024-03-11 15:22:35 +0000

    send_message_hook

---
+++ frontend/src/hooks/useSendMessage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+import useConversation from '../zustand/useConversation';
+import toast from 'react-hot-toast';
+
+const useSendMessage = () => {
+    const [loading,setLoading] =useState(false);
+    const {messages,setMessages,selectConversation}=useConversation();
+
+    const sendMessage = async (message)=>{
+        setLoading(false);
+        try {
+            const res = await fetch(`/api/messages/send/${selectConversation._id}`,{
+                method:'POST',
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                body:JSON.stringify({message})
+            });
+            const data = await res.json();
+            if(data.error){
+                throw new Error(data.error);
+            }
+            setMessages([...messages,data]);
+        } catch (error) {
+            toast.error(error.message);
+        }finally{
+            setLoading(false);
+        }
+    }
+    return {sendMessage,loading}
+}
+
+export default useSendMessage
