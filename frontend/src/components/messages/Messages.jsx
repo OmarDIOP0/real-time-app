8e2c53d9b372567f616f8f33476f756f84ad663d
OmarDIOP0 <odiop6170@gmail.com>
2024-03-12 17:39:40 +0000

    last_message

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Message from './Message'
 import  useGetMessage from '../../hooks/useGetMessage'
 import MessageSkeleton from '../skeleton/MessageSkeleton';
+import { useRef } from 'react';
 
 const Messages = () => {
   const {messages,loading} = useGetMessage();
+  const lastMessage = useRef();
+  useEffect(()=>{
+    setTimeout(()=>{
+      lastMessage.current?.scrollIntoView({behavior:"smooth"});
+    },100)
+  },[messages]);
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
       {!loading && messages.length>0 && 
-       messages.map((message)=><Message key={message._id} message={message}/>)
-      }
+       messages.map((message)=>(
+        <div key={message._id} ref={lastMessage}>
+          <Message message={message}/>
+        </div>
+       ))}
       {loading && [...Array(3)].map((_,idx)=><MessageSkeleton key={idx}/>)}
       {!loading && messages.length === 0 && (
         <p className='text-center text-white'>Send a message to start the conversation</p>
