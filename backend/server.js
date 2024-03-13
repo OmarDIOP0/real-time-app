34308611c5a1cb3f2c5a471fe947d7ed9c897cd1
OmarDIOP0 <odiop6170@gmail.com>
2024-03-13 15:23:59 +0000

    add_socket-io_config_context_

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,10 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from "./db/connexionMongoDB.js";
 import cookieParser from 'cookie-parser';
+import { app , server } from "./socket/socket.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use('/api/users',userRoutes)
 
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
    connectToMongoDB();
    console.log(`Demarrage sur le port ${PORT}`)
 });
---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from 'socket.io-client'
+
+export const SocketContext = createContext();
+
+export const SocketContextProvider = ({children})=>{
+    const [socket,setSocket] = useState(null);
+    const [onlineUsers,setOnlineUsers] =useState([]);
+    const {authUser} = useAuthContext();  
+    useEffect(()=>{
+        if(authUser){
+            const socket = io("http://localhost:5000");
+            setSocket(socket);
+            
+            return ()=> socket.close(); 
+        }
+        else {
+            if(socket){
+                socket.close();
+                setSocket(null);
+            }
+        }
+    },[]);
+    return <SocketContext.Provider value={{socket,onlineUsers}}>{children}</SocketContext.Provider>
+    
+}
